e97a22caf203fc326d235b2e8aba955defb6218b
zhihao2020 <daxu1232018@outlook.com>
2021-01-25 11:44:37 +0800

    change server ini

--- website/settings.py
+++ website/settings.py
@@ -129,12 +129,12 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, Images)s
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 #开发中使用
 STATIC_URL = '/static/'
-#STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT=os.path.join(BASE_DIR,'static_collected')
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
 
